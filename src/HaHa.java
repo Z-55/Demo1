98da4b36982064f24b673b451b0191e28693f50a
zjb000123 <zjb000123@163.com>
2019-10-16 19:33:01 +0800

    11

---
+++ src/HaHa.java
@@ -0,0 +1,5 @@
+public class HaHa {
+    public static void main(String[] args) {
+        System.out.println("....");
+    }
+}
---
+++ src/HeHe.java
@@ -0,0 +1,5 @@
+public class HeHe {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
